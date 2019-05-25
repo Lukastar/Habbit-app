c36bc690f384463c58c5c087f23508d2f2163dc2
Lukasz <startek.lukasz@gmail.com>
2019-05-25 21:39:43 +0200

    "Added a BarChart for overall activity in AllStatScreen"

--- app/src/main/java/com/example/habbit/AllStatScreen.kt
+++ app/src/main/java/com/example/habbit/AllStatScreen.kt
@@ -9,12 +9,17 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.habbit.databinding.AllstatScreenBinding
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.components.Legend
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.lifecycle.Observer
-import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import java.util.*
 
@@ -40,42 +45,41 @@ class AllStatScreen: Fragment(){
         binding.allStatViewModel = allStatViewModel
 
         val pieChart: PieChart = binding.placeholderChart
-        val lineChart: LineChart = binding.allBarchart
+        val barChart: BarChart = binding.allBarchart
         pieChart.setUsePercentValues(true)
 
         val datSet: PieDataSet = PieDataSet(allStatViewModel.chartArray, "Habits")
         datSet.setColors(allStatViewModel.colorArray)
         val pieDat: PieData = PieData(datSet)
 
-        val monthDataSet: LineDataSet = LineDataSet(allStatViewModel.lineArray, "Monthly")
-        monthDataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
-        monthDataSet.fillAlpha = 500
-        monthDataSet.setDrawFilled(true)
-        monthDataSet.fillColor = context!!.getColor(R.color.app_bar_color)
+        val monthDataSet: BarDataSet = BarDataSet(allStatViewModel.lineArray, "Monthly")
         monthDataSet.color = context!!.getColor(R.color.app_bar_color)
-        monthDataSet.setDrawCircles(false)
-        monthDataSet.setDrawValues(false)
-        val monthsSets : ArrayList<ILineDataSet> = ArrayList<ILineDataSet>()
-        monthsSets.add(monthDataSet)
-        val lineDat: LineData = LineData(monthDataSet)
+        monthDataSet.setDrawValues(true)
+        monthDataSet.valueTextSize = 15f
+        val barDat: BarData = BarData(monthDataSet)
 
         pieDat.setValueTextSize(25f)
         pieChart.setEntryLabelTextSize(15f)
         pieChart.setEntryLabelColor(Color.BLACK)
         pieChart.legend.isEnabled = false
-        pieChart.centerText = "All time\nstats"
         pieChart.setCenterTextSize(25f)
         pieChart.data = pieDat
         pieChart.description.isEnabled = false
         pieChart.invalidate()
 
-        lineChart.data = lineDat
-        lineChart.axisLeft.setDrawGridLines(false)
-        lineChart.axisRight.setDrawGridLines(false)
-        lineChart.setDrawGridBackground(false)
-        lineChart.legend.isEnabled = false
-        lineChart.description.isEnabled = false
-        lineChart.invalidate()
+        barChart.data = barDat
+        barChart.axisLeft.setDrawGridLines(false)
+        barChart.axisRight.setDrawGridLines(false)
+        barChart.xAxis.setDrawGridLines(false)
+        barChart.legend.isEnabled = false
+        barChart.description.isEnabled = false
+        barChart.axisRight.isEnabled = false
+        barChart.axisLeft.isEnabled = false
+        barChart.xAxis.labelCount = allStatViewModel.counter+1
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(allStatViewModel.labelArray)
+        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        barChart.xAxis.textSize = 12f
+        barChart.invalidate()
 
         return binding.root
     }
--- app/src/main/java/com/example/habbit/AllStatViewModel.kt
+++ app/src/main/java/com/example/habbit/AllStatViewModel.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.PieEntry
 import kotlinx.coroutines.*
 import java.math.BigDecimal
@@ -24,48 +24,46 @@ class AllStatViewModel (
     var chartArray = ArrayList <PieEntry>()
     var colorArray = ArrayList <Int>()
     var habitColor = ""
-    var lineArray = ArrayList <Entry>()
+    var lineArray = ArrayList <BarEntry>()
+    var labelArray = ArrayList <String>()
     private var whichMonth = ""
     private var monthCounter = 0
     //private val monthHash : HashMap<String,Float> = hashMapOf("Jan" to 1f, "Feb" to 2f, "Mar" to 3f, "Apr" to 4f
         //, "May" to 5f, "Jun" to 6f, "Jul" to 7f, "Aug" to 8f, "Sep" to 9f, "Oct" to 10f, "Nov" to 11f, "Dec" to 12f)
     private var temporaryMonth: BigDecimal = BigDecimal(0)
     private var temporaryYear : BigDecimal = BigDecimal(0)
-    private var counter = 0f
+    var counter = 0
 
     init {
-        dateJoinedList()
-        sortJoinedList()
+        if (joinedList.size != 0) {
+            dateJoinedList()
+            sortJoinedList()
+        }
     }
 
     fun dateJoinedList(){
         println(joinedList2)
         whichMonth = joinedList2[0].day.substring(3,5)
+        labelArray.add(joinedList2[0].day.substring(3,5)+"."+joinedList2[0].day.substring(8,10))
         joinedList2.forEach {
             if (whichMonth.equals(it.day.substring(3,5))) {
-                println("If - counter = "+monthCounter.toString())
                 monthCounter++
 
             }
             else {
-                counter += 1
-                temporaryMonth = (whichMonth.toFloat() / 100).toBigDecimal()
-                //println("TempMonth1: "+temporaryMonth)
-                temporaryYear = it.day.substring(6).toFloat().toBigDecimal()
-                println("tempMonthprint: "+temporaryMonth + " tempYearprint "+temporaryYear)
-                temporaryYear = temporaryYear.plus(temporaryMonth)
-                lineArray.add(Entry(counter,monthCounter.toFloat()))
-                println("Entry = "+ temporaryMonth.toString() + " "+ monthCounter)
+                println("Month" + it.day.substring(3))
+                labelArray.add(it.day.substring(3,5)+"."+it.day.substring(8,10))
+                lineArray.add(BarEntry(counter.toFloat(),monthCounter.toFloat()))
+                println("Else Entry = "+ counter.toString() + " "+ monthCounter)
                 whichMonth = it.day.substring(3,5)
-                monthCounter = 0
-                println("ELSE")
+                counter++
+                monthCounter = 1
             }
         }
-        temporaryMonth = (whichMonth.toFloat() / 100).toBigDecimal()
-        temporaryYear = joinedList2.last().day.substring(6).toFloat().toBigDecimal()
-        temporaryYear = temporaryYear.plus(temporaryMonth)
-        lineArray.add(Entry(counter,monthCounter.toFloat()))
+        lineArray.add(BarEntry(counter.toFloat(),monthCounter.toFloat()))
         lineArray.forEach { println(it) }
+        labelArray.forEach { println(it) }
+
 
     }
 
